634d427e856d768efe688e7573809d01af0c0502
dpavlenko <132207110+EvilHomie@users.noreply.github.com>
2025-09-15 01:41:19 +0300

    feat(DI): добавлен DI контейнер

---
+++ Assets/DI/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DI
+{
+    public class EntryPoint
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void InitGame()
+        {
+            InitDI();
+        }
+
+        private static void InitDI()
+        {
+            var projectInstallerPF = Resources.Load<ProjectInstaller>("ProjectInstaller");
+            var projectInstaller = Object.Instantiate(projectInstallerPF);
+            Object.DontDestroyOnLoad(projectInstaller.gameObject);
+            projectInstaller.Init();
+        }
+    }
+}
---
+++ Assets/DI/ProjectInstaller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace DI
+{
+    public class ProjectInstaller : MonoBehaviour
+    {
+        private Container _container;
+
+        public void Init()
+        {
+            _container = new();
+            InstallBindings();
+            InjectSceneMonobehaviours();
+        }
+
+        private void InjectSceneMonobehaviours()
+        {
+            var sceneMonoBehaviours = FindObjectsByType<MonoBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            foreach (var monoBehaviour in sceneMonoBehaviours)
+            {
+                _container.InjectMonoBehaviour(monoBehaviour);
+            }
+        }
+
+        private void InstallBindings()
+        {
+           
+        }
+    }
+}
